c0d0f2cd9b3b2a88e968d70712476cc0f3fe92d9
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-09-11 17:42:16 +0545

    js workshop day 1

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+const name="Salonee";
+const id=1;
+const status=true;
+const date=new Date();
+console.log(date);
+console.log(date, date.getFullYear());
+console.log(date, date.getTime());
+
+const hd=document.getElementById("hd1").innerHTML = "salonee";
