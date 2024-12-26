55240860e32901fd43bb1da50b96dfb2f144df5c
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-12-26 18:02:07 +0545

    js codes

---
+++ code.js
@@ -0,0 +1,18 @@
+//Node test
+console.log("Hello from JavaScript");
+
+let celeb = {
+    firstName: 'Lola',
+    lastName: 'Tung',
+    series: 'TSITP'
+};
+console.log(celeb.series);
+
+let array  = ['abc123','xy4z56'];
+console.log(array[1]);
+
+//Function to find maximum number
+function maxNumber(num1, num2) {
+    return num1>=num2 ? num1 : num2;
+}
+console.log(maxNumber(4,3));
